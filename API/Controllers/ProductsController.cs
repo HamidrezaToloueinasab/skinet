ad8250d4a897a467c2861bf6b05142989c3eb159
HamidrezaToloueinasab <Hamidrezatolouienasab@gmail.com>
2022-08-23 01:21:42 +0430

    Chapter 4 Second Commit

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -7,34 +7,38 @@ using Core.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Core.Interfaces;
+using Core.Specifications;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class ProductsController : ControllerBase    
+    public class ProductsController : ControllerBase
     {
-        
+
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
         private readonly IGenericRepository<ProductType> _productTypeRepo;
-      
+
         public ProductsController(IGenericRepository<Product> productsRepo, IGenericRepository<ProductBrand> productBrandRepo, IGenericRepository<ProductType> productTypeRepo)
         {
             _productTypeRepo = productTypeRepo;
             _productBrandRepo = productBrandRepo;
             _productsRepo = productsRepo;
-           
-         
+
+
         }
 
         [HttpGet]
-        public async Task <ActionResult<List<Product>>> GetProducts() {
-            var products = await _productsRepo.ListAllAsync();
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var products = await _productsRepo.ListAsync(spec);
             return Ok(products);
         }
-         [HttpGet("{id}")]
-         public async Task<ActionResult<Product>> GetProduct(int id){
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
             return await _productsRepo.GetByIdAsync(id);
         }
         [HttpGet("brands")]
