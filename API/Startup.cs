e07328a447bf36c71ce0b3e5bb2c2fe37c423764
Hamidreza Toloueinasab <Hamidrezatolouienasab@gmail.com>
2022-08-26 01:54:43 +0430

    End of Section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,13 @@ namespace API
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
 
         }
 
@@ -53,6 +60,7 @@ namespace API
             app.UseRouting();
 
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
